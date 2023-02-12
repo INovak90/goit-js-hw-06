3c100b0535a92345ea4647b569d55f30d5cde71d
INovak90 <112341296+INovak90@users.noreply.github.com>
2023-02-12 23:49:01 +0200

    adits2

--- js/task-05.js
+++ js/task-05.js
@@ -1,7 +1,6 @@
 const inputEl = document.querySelector("#name-input");
 const userNameEl = document.querySelector("#name-output");
-
-
+inputEl.value = userNameEl.textContent;
 inputEl.addEventListener("input", function (event) {
   if (event.currentTarget.value) {
     userNameEl.textContent = event.currentTarget.value;
